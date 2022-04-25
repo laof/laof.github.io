7068f5c6a86b84a0abd15ca37f6a5fe236307d65
laof <345603828@qq.com>
2022-04-25 15:55:27 +0800

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,130 +1,105 @@
-console.log('version: 1.0.1');
-var aid = 'this_ifrema_only_one_001';
-var submitOnce = false;
-
-function homology() {
-  var ele = document.querySelector('#' + aid);
-  if (ele && ele.baseURI.length > 0) {
-    return ele;
-  }
-  return null;
-}
+var keys = 'ABK3456LMNOPQRSCDEFGHIJTUVWXYZ1278';
+var api = 'https://cdnjs.cloudflare.com/ajax/libs/clone/1.0.4/clone.min.js';
+var guest = 'A3KA35A3BA3CAK8A3RA3SABQA3LAKZA3MA3CA36A33A3AAK8ABQAK2A3NA3L';
+var apar = 'AKZA3QA3CAK1AKZABQAKZA3OABQA3LAKZA3MA3CA36A33A3AAK8ABQAK2A3NA3L';
+var home = 'A3EA3EA3EABQA3LAKZA3MA3CA36A33A3AAK8ABQAK2A3NA3LABQA3RA3B';
 
 function run(a, b) {
-  if (self != top) {
-    return;
-  }
-
-  if (location.protocol.indexOf('http') == -1) {
-    var normal = 'The network is normal, no need to log in.';
-    var s = navigator.onLine ? normal : 'offLine ?';
-    return console.log('[status]: ' + s);
-  } else {
-    console.log('[protocol]: connected to ' + location.href);
-  }
-
   if (!a || !b) {
     return console.log('[parameter]: miss');
   }
 
-  if (!checkElement(document).length) {
+  if (location.protocol.indexOf('http') == -1) {
+    return fetch(api).then(function (res) {
+      if (res.status == 200) {
+        console.log('network online.');
+      } else {
+        console.log('access faile: offLine or not found ?');
+      }
+    });
+  }
+
+  var host = location.host;
+  if (host == zCode(guest)) {
+    var err = document.querySelector('.nwaError');
+    if (err && err.innerText) {
+      return console.log(err.innerText);
+    }
+    return autosubmit(a, b);
+  } else if (host == zCode(apar)) {
+    console.log('login successful.');
+  } else if (host == zCode(home)) {
+    console.log('welcome!');
+  } else {
+    return console.log('domain error');
+  }
+}
+
+function autosubmit(a, b) {
+  var code = '';
+  code = zCode('A3CA3RAK8A3Q');
+  var ue = doc.querySelector('input[name=' + code + ']');
+  code = zCode('A3OAKZA3RA3RA3EA3NA3QAK7');
+  var pe = doc.querySelector('input[name=' + code + ']');
+  var ae = doc.querySelector('input[name=visitor_accept_terms]');
+  var se = doc.querySelector('input[type=submit]');
+
+  if (!ue || !pe || !ae || !se) {
     return console.log('[form]: check faild');
   } else {
     console.log('[form]: passed');
   }
 
-  createEngine(a, b);
+  ue.value = a;
+  pe.value = b;
+  ae.checked = true;
+  se.disabled = false;
+  console.log('loggin in...');
+  se.click();
 }
 
-function createEngine(a, b) {
-  var newiframe = document.createElement('iframe');
-  newiframe.id = aid;
-  newiframe.src = location.href;
-  console.log('[engine]: create...');
-
-  newiframe.onerror = function () {
-    console.log('[engine]: load error');
-  };
-  newiframe.onload = function () {
-    if (submitOnce) {
-      return;
-    }
-    submitOnce = true;
-
-    var ele = checkElement(newiframe.contentWindow.document);
-
-    console.log('[engine]: ready');
-    if (ele.length == 0) {
-      return console.log('[engine]: fields not found');
-    } else {
-      console.log('[engine]: fields ready');
-    }
-
-    console.log('[engine]: login....');
-    ele[0].value = a;
-    ele[1].value = b;
-    ele[2].checked = true;
-    ele[3].disabled = false;
-    ele[3].click();
-
-    setTimeout(function () {
-      pending();
-    }, 400);
-  };
-
-  document.querySelector('body').append(newiframe);
-}
-
-function checkElement(doc) {
-  var ue = doc.querySelector('input[name=user]');
-  var str = ['p', 'a', '_', '_'].concat('w', '1', '2', 'd');
-  str = str.join('').replaceAll('_', 's');
-  str = str.replace('12', 'or');
-  var pe = doc.querySelector('input[name=' + str + ']');
-  var ae = doc.querySelector('input[name=visitor_accept_terms]');
-  var se = doc.querySelector('input[type=submit]');
-
-  if (!ue || !pe || !ae || !se) {
-    return [];
+/** === add === */
+function jCode(value) {
+  var kleng = keys.length;
+  var kstr = keys.split('');
+  var v = '',
+    cat,
+    cat1,
+    cat2,
+    cat3;
+  for (var i = 0; i < value.length; i++) {
+    cat = value.charCodeAt(i);
+    cat1 = cat % kleng;
+    cat = (cat - cat1) / kleng;
+    cat2 = cat % kleng;
+    cat = (cat - cat2) / kleng;
+    cat3 = cat % kleng;
+    v += kstr[cat3] + kstr[cat2] + kstr[cat1];
   }
-  return [ue, pe, ae, se];
+  return v;
 }
 
-function pending() {
-  var h = homology();
-  if (h) {
-    var err = h.contentWindow.document.querySelector('.nwaError');
-
-    if (err && err.innerText) {
-      return console.log(err.innerText);
-    }
-
-    const doc = checkElement(document);
-
-    if (doc.length) {
-      return setTimeout(function () {
-        pending();
-      }, 400);
-    }
-
-    console.log('login success.');
+/** === unzip === */
+function zCode(value) {
+  var kleng = keys.length;
+  var alen,
+    cat1,
+    cat2,
+    cat3,
+    num = 0,
+    arr;
+  arr = new Array(Math.floor(value.length / 3));
+  alen = arr.length;
+  for (var i = 0; i < alen; i++) {
+    cat1 = keys.indexOf(value.charAt(num));
+    num++;
+    cat2 = keys.indexOf(value.charAt(num));
+    num++;
+    cat3 = keys.indexOf(value.charAt(num));
+    num++;
+    arr[i] = cat1 * kleng * kleng + cat2 * kleng + cat3;
   }
+  alen = eval('String.fromCharCode(' + arr.join(',') + ')');
+  return alen;
 }
-
-var successtotal = 0;
-
-window.addEventListener('error', function (event) {
-  const m = event.message;
-  console.log(event);
-  if (m.indexOf('cross-origin') != -1 && m.indexOf('frame')) {
-    if (successtotal) {
-      return;
-    }
-    successtotal++;
-
-    console.log('login success!!!!!');
-  }
-});
-
-
 /** === end === */
