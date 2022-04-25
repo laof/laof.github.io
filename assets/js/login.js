9ac1ed15e43a15767b7da2535971add481a52035
laof <345603828@qq.com>
2022-04-25 18:35:07 +0800

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -19,6 +19,7 @@ function run(a, b) {
 
   var host = location.host;
   if (host == zCode(guest)) {
+    console.log('wait...');
     var err = document.querySelector('.nwaError');
     if (err && err.innerText) {
       return console.log(err.innerText);
