946205ec19e1c4424456bbc68b2985ded857224a
laof <345603828@qq.com>
2022-04-30 22:47:21 +0800

    add

--- motuo/ssr.js
+++ motuo/ssr.js
@@ -1,6 +1,6 @@
 function getList(text, key) {
-  const values = []
-  const reg = {
+  var values = []
+  var reg = {
     ss: /ss:\/\/(.*?)(?=[\r\n,]|<\/)/g,
     ssr: /ssr:\/\/(.*?)(?=[\r\n,]|<\/)/g,
     vmess: /vmess:\/\/(.*?)(?=[\r\n,]|<\/)/g,
@@ -10,10 +10,10 @@ function getList(text, key) {
     return values
   }
 
-  const vs = text.match(reg[key]) || []
+  var vs = text.match(reg[key]) || []
 
   if (vs.length) {
-    vs.forEach((item, index) => {
+    vs.forEach(function (item, index) {
       values.push({
         name: key + index,
         value: item,
