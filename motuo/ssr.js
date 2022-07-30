71254ea2708595f16293a72a068d533123d3dd21
denglan <18030893186@163.com>
2022-07-30 23:06:57 +0800

    add

--- motuo/ssr.js
+++ motuo/ssr.js
@@ -24,7 +24,7 @@ function getList(text, key) {
   return values
 }
 
-function run() {
+function run1() {
   var ssr = getList(document.body.innerHTML, 'ssr')
   var ss = getList(document.body.innerHTML, 'ss')
   var total = [].concat(ssr, ss)
@@ -42,6 +42,31 @@ function run() {
     console.log(log.join(' ') + ', please to copy')
   } else {
     console.log('not found data, please upgrade job.')
+  }
+  return txt
+}
+
+// https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7
+
+var noData = 'not found data, please upgrade job.'
+
+function run() {
+
+  var pre = document.querySelector('pre')
+
+  if (!pre || !pre.innerHTML) {
+    console.log(noData)
+    return ''
+  }
+
+  var txt = pre.innerHTML
+
+  var list = txt.split('\n')
+  if (list.length) {
+    console.log(list.join('\n\n'))
+    console.log('total:' + list.length + ', please to copy')
+  } else {
+    console.log(noData)
   }
   return txt
 }
